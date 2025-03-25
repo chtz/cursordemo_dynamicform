33a5edc1981a68a5cb900649b5751ef5eaa023bf
Christian Tschenett <cht@zuehlke.com>
2025-03-25 21:44:07 +0100

    gh pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/cursordemo_dynamicform/',
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 5174,
