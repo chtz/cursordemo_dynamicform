1403bd4674b4b63e3496055a2c3145752d559b3d
Christian Tschenett <cht@zuehlke.com>
2025-03-25 21:28:25 +0100

    gh pages (wip)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cursordemo_dynamicform/',
   server: {
     host: '0.0.0.0',
     port: 5174,
